0cd71e24cd501fa9545af31cf504c1a5b76bfcad
Mian Akbar Jan <mianakbarjan@hotmail.com>
2022-09-22 17:54:21 +0500

    Fixed errors in Lab 2 and added comments

--- Lab-2/task2.cpp
+++ Lab-2/task2.cpp
@@ -7,26 +7,42 @@ int rollNo;
 string name;
 int age;
 string address;
+void enterData(){
+     cout << "Student Roll No.: ";
+cin >> rollNo;
+cin.ignore();
+cout << "Student Name: ";
+getline(cin, name);
+cout << "Student Age: ";
+cin >> age;
+if (age<11 || age>14){
+    cout <<"Enter age again: ";
+    cin >> age;
+}
+cin.ignore();
+cout << "Student Address: ";
+getline(cin, address);
+}
 };
 //checks students with age 14 and prints their name
-void age14(student *s){
+void age14(student *s, int size){
     int i=0;
     do {
         if (s[i].age==14){
 cout << "Student name: " << s[i].name << endl;
         }
 i++;
-    }while (s[i].age);
+    }while (i<size);
 }
 //checks students with even rollNo.s and prints their names
-void even(student *s){
+void even(student *s, int size){
     int i=0;
     do {
 if (s[i].rollNo%2==0){
     cout << "Student name: " << s[i].name << endl;
 }
 i++;
-    }while (s[i].rollNo);
+    }while (i<size);
 }
 //takes in a rollNo and finds it in list
 void rollNum(student *s, int size){
@@ -48,20 +64,8 @@ int main(){
     int size = 1;
     student *s = new student [size];
     char option;
-    cout << "Student Roll No.: ";
-cin >> s[0].rollNo;
-cin.ignore();
-cout << "Student Name: ";
-getline(cin, s[0].name);
-cout << "Student Age: ";
-cin >> s[0].age;
-if (s[0].age<11 || s[0].age>14){
-    cout <<"Enter age again: ";
-    cin >> s[0].age;
-}
-cin.ignore();
-cout << "Student Address: ";
-getline(cin,s[0].address);
+    s[size-1].enterData();
+
 do {
 cout << "Do you want to add a new student: ";
 cin >> option;
@@ -77,27 +81,14 @@ if (option=='y' || option == 'Y'){
 delete []s;
 s=temp;
 cout << "Enter details of student " << size << ": ";
-cout << "Student Roll No.: ";
-cin >> s[size-1].rollNo;
-cin.ignore();
-cout << "Student Name: ";
-getline(cin, s[size-1].name);
-cout << "Student Age: ";
-cin >> s[size-1].age;
-if (s[size-1].age<11 || s[size-1].age>14){
-    cout <<"Enter age again: ";
-    cin >> s[size-1].age;
-}
-cin.ignore();
-cout << "Student Address: ";
-getline(cin, s[size-1].address);
+s[size-1].enterData();
 }
 else {break;}
 
 }while (option!='n' || option!='N');
 
-age14(s);
-even(s);
+age14(s, size);
+even(s, size);
 
 rollNum(s, size);
 return 0;
