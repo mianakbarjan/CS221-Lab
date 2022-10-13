53bc1af9a735f3d9af3514e414d21af94bd7e7eb
Mian Akbar Jan <mianakbarjan@hotmail.com>
2022-10-13 13:19:09 +0500

    Tasks on Queues

---
+++ Lab-5/practice.cpp
@@ -0,0 +1,43 @@
+#include "Queue.h"
+void menu(){
+    cout << "1. Enqueue" << endl;
+    cout << "2. Dequeue" << endl;
+    cout << "3. Display All" << endl;
+    cout << "0. Exit" << endl;
+    cout << "Enter Option: ";
+}
+
+int main(){
+    Queue<int> q;
+    int option;
+    do {
+        menu();
+        cin >> option;
+        switch (option){
+            case 1: {
+                int insertValue;
+                cout << "Enter value to enter: ";
+                cin >> insertValue;
+                q.enqueue(insertValue);
+                break;
+            }
+            case 2:{
+                q.dequeue();
+                break;
+            }
+            case 3:
+            {
+                q.displayAll();
+                break;
+            }
+            case 0: 
+            {
+                break;
+            }
+            default:{
+                cout << "Enter correct value: ";
+            }
+        }
+    }while (option!=0);
+    return 0;
+}
